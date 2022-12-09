f59eec66d9a889542a0cc482bd3e6e6fc58ca561
BrianAsaph <asaphkahesh11@gmail.com>
2022-12-09 15:12:07 +0000

    bes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def best_score(a_dictionary):
+    if a_dictionary and len(a_dictionary):
+        max = list(a_dictionary.keys())[0]
+        for key in a_dictionary:
+            if a_dictionary[key] > a_dictionary[max]:
+                max = key
+        return max
+    return None
